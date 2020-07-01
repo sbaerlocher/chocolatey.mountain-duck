0ef60411990b83b45701d2d08035facfdd523570
avarins <avarins@sbaerlo.ch>
2020-07-01 19:08:31 +0000

    chocomilk: Update to 4.0.1.16800

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters = Get-PackageParameters
-$urlPackage = "https://dist.mountainduck.io/Mountain%20Duck%20Installer-4.0.0.16759.msi"
-$checksumPackage = "993641b3682a339b36592729dd9b3eab2875bfb52e3a8951c85e5204bd9f83c74e662923eb2d469e59f70ff2fecd20fe44701b68759ecaecdcee3a627251aac6"
+$urlPackage = "https://dist.mountainduck.io/Mountain%20Duck%20Installer-4.0.1.16800.msi"
+$checksumPackage = "ce3cbad4ddb28930b713d830ff3ca5af7c436952b6a9f41d5d6ebb42e0945c97cd0dfe6d6a09aa5947abb36b9af6636d49958c0e8af9ec5f0bbf03530ace34e6"
 $checksumTypePackage = 'SHA512'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
